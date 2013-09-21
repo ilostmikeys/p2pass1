ae550fea74f2e167eebb74a25e025e571d8c7cab
Michael Vuong <mvuo803@cse.unsw.edu.au>
2013-09-21 14:57:25 +1000

    added in perl test folder

--- perl2python.pl
+++ perl2python.pl
@@ -4,15 +4,27 @@ use diagnostics;
 use strict;
 
 foreach my $file ($ARGV[0]) { 
-	extensionChecker($file);
-}
-sub extensionChecker { 
-		my $fileToCheck = shift;
-		my $extension = "";
-		$extension .= $fileToCheck; 
-		$extension =~ s/.*\.//g;
-	
-		print "Wrong extension \n", "Usage: $0 fileName.pl\n" if $extension ne "pl";
+	extensionChecker ($file);
+	hashbang($file);
 }
 
-	
+sub extensionChecker { 
+	my $fileToCheck = $_[0];
+	print "$fileToCheck\n";
+	my $extension = "";
+	$extension .= $fileToCheck; 
+	$extension =~ s/.*\.//g;
+	if (!$extension eq "pl") { 
+		print "Wrong extension \n", "Usage: $0 fileName.pl\n" if $extension ne "pl";
+		exit(1);
+	}
+}
+
+sub hashbang { 
+	my $hb = shift;
+	if ($hb =~ /#!\/usr\/bin\/python/) { 
+		print "Hashbang";
+	}
+}
+
+
